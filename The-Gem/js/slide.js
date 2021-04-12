c363cf9f275373b299b635bf30b9f70a45a3afca
thien021998 <tieuquy16021998@gmail.com>
2021-04-12 23:23:23 +0700

    edit js and add button scroll to top

--- The-Gem/js/slide.js
+++ The-Gem/js/slide.js
@@ -1,88 +1,70 @@
 var slideIndex = 0;
-var slideIndex1 = 1;
-showSlides1(slideIndex1);
-showSlides();
-showSlides2()
-function currentSlide(n) {
-  showSlides1(slideIndex1 = n);
+show()
+
+function show(){
+showSlides(slideIndex);
+setTimeout(show, 2000);
 }
-function currentSlide1(n) {
-  showSlides3(slideIndex1 = n);
-}
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-function showSlides() {
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  console.log("sli1",slideIndex)
-  console.log("length",slides.length)
-  if (slideIndex > slides.length) {
-    slideIndex = 1
-    console.log("sli2",slideIndex)
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  console.log(slideIndex)
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-function showSlides2() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides1");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  console.log("sli1",slideIndex)
-  console.log("length",slides.length)
-  if (slideIndex > slides.length) {
-    slideIndex = 1
-    console.log("sli2",slideIndex)
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  console.log(slideIndex)
-  setTimeout(showSlides2, 2000); // Change image every 2 seconds
-}
-function showSlides1(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex1 = 1}
-  if (n < 1) {slideIndex1 = slides.length}
+    var slides1 = document.getElementsByClassName("mySlides1");
+  var dots1 = document.getElementsByClassName("dot1");
+  if (n > slides.length) {slideIndex = 1}
+  if (n > slides1.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  if (n < 1) {slideIndex = slides1.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex1-1].style.display = "block";
-  dots[slideIndex1-1].className += " active";
+  for (i = 0; i < slides1.length; i++) {
+      slides1[i].style.display = "none";
+  }
+  for (i = 0; i < dots1.length; i++) {
+      dots1[i].className = dots1[i].className.replace(" active", "");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    if (slideIndex > slides1.length) {slideIndex = 1}
+  if (slideIndex < 1) {slideIndex = slides.length}
+  console.log(slideIndex)
+  if (slideIndex < 1) {slideIndex = slides1.length}
+  console.log(slideIndex)
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  slides1[slideIndex-1].style.display = "block";
+  dots1[slideIndex-1].className += " active";
 }
-function showSlides3(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides1");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex1 = 1}
-  if (n < 1) {slideIndex1 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex1-1].style.display = "block";
-  dots[slideIndex1-1].className += " active";
+
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+console.log(document.documentElement.scrollTop,document.body.scrollTop)
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
