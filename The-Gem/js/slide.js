871d14fc2f51ae97566a071e4a5d32153950bdf8
thien021998 <tieuquy16021998@gmail.com>
2021-04-15 00:05:35 +0700

    fix file css and js

--- The-Gem/js/slide.js
+++ The-Gem/js/slide.js
@@ -42,27 +42,27 @@ function showSlides(n) {
 };
 
 
-var mynav = document.getElementById("fixnav");
-var mylogo = document.getElementsByClassName("logo-text")
-var mylink = document.getElementById("item-header")
-var mybutton = document.getElementById("myBtn");
-window.onscroll = function () { scrollFunction() };
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-    mynav.style.background = "white";
-    mylink.style.border = "3px ridge black"
-    mylogo[0].style.color = "black";
-    mylogo[0].style.color = "black";
-  } else {
-    mybutton.style.display = "none";
-    mynav.style.background = "#2c2e3d";
-    mylink.style.border = "3px ridge white"
-    mylogo[0].style.color = "white";
-  }
-};
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-};
+// var mynav = document.getElementById("fixnav");
+// var mylogo = document.getElementsByClassName("logo-text")
+// var mylink = document.getElementById("item-header")
+// var mybutton = document.getElementById("myBtn");
+// window.onscroll = function () { scrollFunction() };
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//     mynav.style.background = "white";
+//     mylink.style.border = "3px ridge black"
+//     mylogo[0].style.color = "black";
+//     mylogo[0].style.color = "black";
+//   } else {
+//     mybutton.style.display = "none";
+//     mynav.style.background = "#2c2e3d";
+//     mylink.style.border = "3px ridge white"
+//     mylogo[0].style.color = "white";
+//   }
+// };
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// };
 
